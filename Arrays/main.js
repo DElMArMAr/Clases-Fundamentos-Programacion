0f44e5a15467a7b8e4b5f40df20e6bd3ec4caea7
DElMArMAr <salazarmariadn14@gmail.com>
2026-04-15 16:55:35 -0600

    Add files via upload

---
+++ Arrays/main.js
@@ -0,0 +1,56 @@
+//array
+
+//definir array
+
+//const list = ['manzana', 'pera', 'uva', 'piña'];
+//const randomList = ['pera', 32 , false ['uva']];
+//let empty = []
+
+// acceder a los elementos de un array
+
+//const fruit = list[3];
+//console.log(list.length);
+//console.log(fruit);
+
+//Agragar elemento a la lista 
+
+//list.push('naranja');
+//list.push('tomate');
+
+
+//eliminar elemntos 
+
+//list.splice(2, 1);
+//console.log(list);
+
+// Loops 
+
+//for(let i = 0; i < list.length; i++) {
+//console.log(list[i]);};
+
+
+
+ //output
+ //pares:[]
+ //impares[]
+
+const list = ['32', '87', '999', '1', '40', '111'];
+const pares = []
+const impares = []
+
+
+for(let i = 0; i < list.length, i++); {
+ 
+if (list[i] % 2 === 0) {
+    pares.push(list[i]); 
+   
+} else {
+    impares.push(list[i]); 
+}
+};
+
+console.log('Pares:', pares );
+console.log('Impares:', pares );
+
+//.map
+
